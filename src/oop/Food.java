2682df02cfa4c77ae7cf847621ecf854948fbfc9
Pikott143 <Theunseenmaiden@gmail.com>
2023-11-17 13:59:15 +0800

    Created sample project

---
+++ src/oop/Food.java
@@ -0,0 +1,20 @@
+package oop;
+
+class Food {
+    String food_name;
+       double food_price;
+       
+       Food(String food_name,double food_price){
+           this.food_name = food_name;
+           this.food_price = food_price;
+    }
+    
+       void showFoodName(){
+           System.out.println(food_name);
+       }
+         void showFoodPrice(){
+           System.out.println(food_price);
+       }
+}
+
+
--- src/oop/Main.java
+++ src/oop/Main.java
@@ -3,7 +3,10 @@ package oop;
 public class Main {
 
     public static void main(String[] args) {
-      System.out.println("THANK YOU");
+      Food food_00 = new Food ("turon", 10.00);
+      
+      food_00.showFoodName();
+      food_00.showFoodPrice();
     }
     
 }
